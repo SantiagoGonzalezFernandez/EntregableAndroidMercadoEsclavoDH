37948d152ee46edd5452459cd64525f38a5fddff
Santiago <SantiagoGonzalezFernandezpro@gmail.com>
2019-12-08 20:09:37 -0300

    ToolBar 80% (Faltan algunas Logicas) , LogOut 100%
    falta el logIn de facebook y el logOut de facebook

--- app/src/main/java/com/santiagogonzalez/mercadoesclavodh/MainActivity.java
+++ app/src/main/java/com/santiagogonzalez/mercadoesclavodh/MainActivity.java
@@ -1,14 +1,146 @@
 package com.santiagogonzalez.mercadoesclavodh;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar myToolbar;
+    private DrawerLayout myDrawerLayout;
+    private NavigationView myNavigationView;
+
+    private SearchView mySearchView;
+
+    private FirebaseAuth myFirebaseAuth; //El firebase Auth te da las cosas del user
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        encuentroComponentesPorId();
+
+        configuroToolbar();
+    }
+
+    private void encuentroComponentesPorId() {
+        myToolbar = findViewById(R.id.MainActivity_Include_Toolbar);
+        myDrawerLayout = findViewById(R.id.MainActivity_DrawerLayout_Contenedor);
+        myNavigationView = findViewById(R.id.MainActivity_NavigationView);
+    }
+
+    private void configuroToolbar() {
+        setSupportActionBar(myToolbar);
+
+        ActionBarDrawerToggle myActionBarDrawerToggle =
+                new ActionBarDrawerToggle(this,
+                        myDrawerLayout,
+                        myToolbar,
+                        R.string.open_drawer,
+                        R.string.close_drawer); //Animacion de la hamburguesa (Open, Close Drawer)
+
+        myDrawerLayout.addDrawerListener(myActionBarDrawerToggle);
+        myActionBarDrawerToggle.syncState();
+
+        //le seteo el onClick,le digo this por que la MainActivity implementa el listener (NavigationView.OnNavigationItemSelectedListener)
+        myNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    //Este metodo se tiene que implementar por (NavigationView.OnNavigationItemSelectedListener) para configurar los items Del NavigationView
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int intItemSeleccionadoId = menuItem.getItemId();
+
+        switch (intItemSeleccionadoId) {
+            case R.id.MenuPrincipal_Item_Home:
+                Toast.makeText(this, "Home", Toast.LENGTH_LONG).show();
+                //Aca va la logica del home
+                break;
+            case R.id.MenuPrincipal_Item_Perfil:
+                Toast.makeText(this, "Perfil", Toast.LENGTH_LONG).show();
+                //Aca va la logica de Perfil
+                break;
+            case R.id.MenuPrincipal_Item_AboutUs:
+                Toast.makeText(this, "About Us", Toast.LENGTH_LONG).show();
+                //Aca va la logica de About Us
+                break;
+            case R.id.MenuPrincipal_Item_CerrarSesion:
+                Toast.makeText(this, "Cerrar sesion", Toast.LENGTH_LONG).show();
+                //Cierro la sesion de firebase y abro el LoginActivity
+                myFirebaseAuth = FirebaseAuth.getInstance();
+                myFirebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                break;
+        }
+        myDrawerLayout.closeDrawers();
+        return true;
+    }
+
+    //Este metodo te lo da la extencion con AppCompatActivity
+    //En este caso lo vamos a usar para configurar una parte de la Toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflo el toolbar
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        //Encuentro los componentes del menu
+        MenuItem myMenuItemSearch = menu.findItem(R.id.ToolBarMenu_Item_action_search);
+        MenuItem myMenuItemProfile = menu.findItem(R.id.ToolBarMenu_Item_perfilUser);
+
+        //Configuro que pasa si se clickea el Perfil
+        myMenuItemProfile.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.ToolBarMenu_Item_perfilUser) {
+                    Toast.makeText(getApplicationContext(), "Perfil", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+
+        mySearchView = (SearchView) myMenuItemSearch.getActionView();
+        mySearchView.setQueryHint("Que buscas?");
+
+        /*
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                controllerPelicula.traerPeliculasPorBusqueda(query, new ResultListener<List<Pelicula>>() {
+                    @Override
+                    public void finish(List<Pelicula> result) {
+                        adapterPelicula.setPeliculaList(result);
+                    }
+                });
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myArrayAdapterString.getFilter().filter(newText);
+                return true;
+            }
+        });
+        */
+
+        return true;
     }
 }
